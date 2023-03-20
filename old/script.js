f8c03bf18a5d42bb8d87d54e0e9e6ec3dbb26817
Silva <francisilva@deloitte.pt>
2023-03-20 09:37:35 +0000

    first commit

---
+++ old/script.js
@@ -0,0 +1,7 @@
+function mostrarPopUp() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function fecharPopUp() {
+    document.getElementById("popup").style.display = "none";
+}
